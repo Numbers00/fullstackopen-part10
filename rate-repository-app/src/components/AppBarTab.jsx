55a44775c7d87049d90a940a8bdb46a2c0213ec8
Numbers00 <kmaquino@up.edu.ph>
2023-08-02 19:57:26 +0800

    Upload for submission up to exer 10.4

---
+++ rate-repository-app/src/components/AppBarTab.jsx
@@ -0,0 +1,26 @@
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+
+import { Pressable, StyleSheet, View } from 'react-native';
+
+import Text from './Text';
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  }
+});
+
+const AppBarTab = ({ text }) => {
+  return (
+    <Pressable onPress={() => console.log('pressed')}>
+      <View style={styles.container}>
+        <View />
+        <MaterialCommunityIcons name="source-repository" size={36} color="white" />
+        <Text isWhite fontWeight="bold">{ text }</Text>
+      </View>
+    </Pressable>
+  );
+};
+
+export default AppBarTab;
