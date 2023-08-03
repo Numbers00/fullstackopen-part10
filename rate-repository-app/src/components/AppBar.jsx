b4172b87efdb4cc29ecfde24936416cca587004e
Numbers00 <kmaquino@up.edu.ph>
2023-08-03 13:21:07 +0800

    Upload for submission up to exer 10.7

--- rate-repository-app/src/components/AppBar.jsx
+++ rate-repository-app/src/components/AppBar.jsx
@@ -1,4 +1,4 @@
-import { View, StyleSheet } from 'react-native';
+import { ScrollView, StyleSheet, View } from 'react-native';
 import Constants from 'expo-constants';
 
 import AppBarTab from './AppBarTab';
@@ -16,8 +16,10 @@ const styles = StyleSheet.create({
 const AppBar = () => {
   return (
     <View style={styles.container}>
-      <AppBarTab text='Repositories' to='/' />
-      <AppBarTab text='Sign In' to='/sign-in' />
+      <ScrollView horizontal>
+        <AppBarTab text='Repositories' to='/' />
+        <AppBarTab text='Sign In' to='/sign-in' />
+      </ScrollView>
     </View>
   );
 };
